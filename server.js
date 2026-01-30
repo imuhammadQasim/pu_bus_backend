b1e9f816e5de453eb215cc41974d02679ccc81df
muhammad <mq80140@gmail.com>
2026-01-30 23:35:21 +0500

    added morgan and log file

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require("./app");
 const config = require("./config/env");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/health", (req, res) => {
   res.json({ status: "okay" });
